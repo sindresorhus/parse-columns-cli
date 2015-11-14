608d1a1f58d60a93caf401eeae257c059bbfd37f
Sindre Sorhus <sindresorhus@gmail.com>
2015-11-14 19:09:41 +0700

    tweaks - minimum Node.js version is now v4

--- cli.js
+++ cli.js
@@ -1,25 +1,23 @@
 #!/usr/bin/env node
 'use strict';
-var getStdin = require('get-stdin');
-var meow = require('meow');
-var parseColumns = require('parse-columns');
+const getStdin = require('get-stdin');
+const meow = require('meow');
+const parseColumns = require('parse-columns');
 
-var cli = meow({
-	help: [
-		'Usage',
-		'  $ parse-columns <string>',
-		'  $ echo <string> | parse-columns',
-		'',
-		'Example',
-		'  $ lsof -i | parse-columns | underscore --outfmt=text pluck NAME',
-		'  *:*',
-		'  *:62886',
-		'  *:*',
-		'  localhost:9050 (LISTEN)'
-	]
-});
+const cli = meow(`
+	Usage
+	  $ parse-columns <string>
+	  $ echo <string> | parse-columns
 
-var input = cli.input[0];
+	Example
+	  $ lsof -i | parse-columns | underscore --outfmt=text pluck NAME
+	  *:*
+	  *:62886
+	  *:*
+	  localhost:9050 (LISTEN)
+`);
+
+const input = cli.input[0];
 
 function init(data) {
 	console.log(JSON.stringify(parseColumns(data)));
@@ -33,5 +31,5 @@ if (!input && process.stdin.isTTY) {
 if (input) {
 	init(input);
 } else {
-	getStdin(init);
+	getStdin().then(init);
 }
--- test.js
+++ test.js
@@ -1,25 +1,13 @@
-'use strict';
-var test = require('ava');
-var childProcess = require('child_process');
+import childProcess from 'child_process';
+import test from 'ava';
+import pify from 'pify';
 
-test('main', function (t) {
-	t.plan(2);
-
-	childProcess.execFile('./cli.js', ['foo bar\n1   2'], {
-		cwd: __dirname
-	}, function (err, stdout) {
-		t.assert(!err, err);
-		t.assert(JSON.parse(stdout)[0].foo === '1');
-	});
+test('main', async t => {
+	const stdout = await pify(childProcess.execFile)('./cli.js', ['foo bar\n1   2']);
+	t.is(JSON.parse(stdout)[0].foo, '1');
 });
 
-test('stdin', function (t) {
-	t.plan(2);
-
-	childProcess.exec('echo \'foo bar\n1   2\' | ./cli.js', {
-		cwd: __dirname
-	}, function (err, stdout) {
-		t.assert(!err, err);
-		t.assert(JSON.parse(stdout)[0].foo === '1');
-	});
+test('stdin', async t => {
+	const stdout = await pify(childProcess.exec)('echo \'foo bar\n1   2\' | ./cli.js');
+	t.is(JSON.parse(stdout)[0].foo, '1');
 });
